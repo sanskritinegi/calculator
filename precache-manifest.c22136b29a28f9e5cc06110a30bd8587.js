67eba80ca5b12d0f057cf764334207204997a87a
sanskriti <sanskritinegi37@gmail.com>
2020-04-17 17:01:46 +0530

    Updates

---
+++ precache-manifest.c22136b29a28f9e5cc06110a30bd8587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e53df197f98cd8d1cad35f872dc64bf",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "f9b42222aa733b1482cf",
+    "url": "/calculator/static/css/main.731e933d.chunk.css"
+  },
+  {
+    "revision": "ec61145f7cc58095d60b",
+    "url": "/calculator/static/js/2.ab148f88.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.ab148f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b42222aa733b1482cf",
+    "url": "/calculator/static/js/main.0002b89d.chunk.js"
+  },
+  {
+    "revision": "552c96952084d63edc80",
+    "url": "/calculator/static/js/runtime-main.29434a71.js"
+  }
+]);
